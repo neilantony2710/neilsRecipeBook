3348c7727533e364a8aa3d432c0672a8f0c65b95
Neil  Antony <neilantony@169-231-44-22.wireless.ucsb.edu>
2024-01-19 02:30:26 -0800

    Implemented SQL

--- app.py
+++ app.py
@@ -1,93 +1,130 @@
 from flask import *
 from flask_bootstrap import Bootstrap
-from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = "mongodb://neil1:neil1234@ds113853.mlab.com:13853/neilantony2710login?retryWrites=false"
-app.config['SECRET_KEY'] = 'secret'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///neilsrb.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "secret"
 Bootstrap(app)
-mongo = PyMongo(app)
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+# User Information Schema
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    fname = db.Column(db.String(100))
+    lname = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, fname, lname, email, password):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.password = password
+
+# Recipe Schema
+class recipies(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    cookTime = db.Column(db.Integer)
+    hardness = db.Column(db.String(100))
+    instructions = db.Column(db.String(5000))
+    user = db.Column(db.String(100))
+
+    def __init__(self, title, cookTime, hardness, instructions, user):
+        self.title = title
+        self.cookTime = cookTime
+        self.hardness = hardness
+        self.instructions = instructions
+        self.user = user
+with app.app_context():
+    db.create_all()
+@app.route("/", methods=["GET", "POST"])
 def home():
-    if 'login' in session:
-        if request.method == 'GET':
-            recipies = list(mongo.db.rbRecipies.find())
-            for recipie in recipies:
-                if recipie['hardness'] == 'Difficult':
-                    recipie['color'] = 'bg-danger'
-                elif recipie['hardness'] == 'Medium':
-                    recipie['color'] = 'bg-warning'
+    if "login" in session:
+        if request.method == "GET":
+            Xrecipies = list(recipies.query.all())
+            print(len(Xrecipies))
+            for recipie in Xrecipies:
+                if recipie.hardness == "Difficult":
+                    recipie.color = "bg-danger"
+                elif recipie.hardness == "Medium":
+                    recipie.color = "bg-warning"
                 else:
-                    recipie['color'] = 'bg-success'
-            return render_template('index.html', r=recipies, ab='1')
-        elif request.method == 'POST':
-            inputRecipe = {}
-            inputRecipe['title'] = request.form['title']
-            inputRecipe['cookTime'] = request.form['cookTime']
-            inputRecipe['hardness'] = request.form['hardness']
-            inputRecipe['instructions'] = request.form['instructions']
-            inputRecipe['user'] = session['login']
-            print(inputRecipe)
-            mongo.db.rbRecipies.insert_one(inputRecipe)
-            return redirect('/')
+                    recipie.color = "bg-success"
+            return render_template("index.html", r=Xrecipies, ab="1")
+        elif request.method == "POST":
+            Xtitle = request.form["title"]
+            XcookTime = request.form["cookTime"]
+            Xhardness = request.form["hardness"]
+            Xinstructions = request.form["instructions"]
+            Xuser = session["login"]
+            rcp = recipies(Xtitle,XcookTime,Xhardness,Xinstructions,Xuser)
+            db.session.add(rcp)
+            db.session.commit()
+            return redirect("/")
     else:
-        return redirect('/login')
-@app.route('/register', methods=['GET', 'POST'])
+        return redirect("/login")
+
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'GET':
-        return render_template('register.html')
-    if request.method == 'POST':
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
         y = {}
-        y['fname'] = request.form['firstname'].title()
-        y['lname'] = request.form['lastname'].title()
-        y['email'] = request.form['email']
-        y['password'] = request.form['password']
-        y['following'] = []
-        y['followers'] = []
-        y['schedule'] = {}
-        print(y)
-        x = mongo.db.rbUser.find_one({'email': y['email']})
+        y["fname"] = request.form["firstname"].title()
+        y["lname"] = request.form["lastname"].title()
+        y["email"] = request.form["email"]
+        y["password"] = request.form["password"]
+
+        x = users.query.filter_by(email=y["email"]).first()
+        print(x)
+        # Checks if user is already registered based on email val
         if x is not None:
-            flash('Account with E-Mail already created.')
-            return redirect('/login')
-        mongo.db.rbUser.insert_one(y)
-        flash('Account created, Now login')
-        return redirect('/login')
+            flash("Account with E-Mail already created.")
+            return redirect("/login")
+
+        usr = users(y["fname"],y["lname"],y["email"],y["password"])
+        print(usr.email)
+        db.session.add(usr)
+        db.session.commit()
+        flash("Account created, Now login")
+        return redirect("/login")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'GET':
-        if 'login' in session:
-            flash('You are already logged in.')
-            return redirect('/')
+    if request.method == "GET":
+        if "login" in session:
+            flash("You are already logged in.")
+            return redirect("/")
         else:
-            return render_template('login.html')
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        y = mongo.db.rbUser.find_one({'email': email, 'password': password})
+            return render_template("login.html")
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        print(email)
+        y = users.query.filter_by(email=email).first()
+        print(y)
         if y is None:
-
-            flash('incorrect information')
-            return redirect('/login')
+            flash("incorrect information")
+            return redirect("/login")
         else:
+            session["login"] = email
+            flash("Success!")
+        return redirect("/")
 
-            session['login'] = email
 
-
-            flash('Success!')
-        return redirect('/')
-
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     session.clear()
-    return redirect('/login')
+    return redirect("/login")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
-
-# deploy using heroku, look at student portal, send link to Rohit after.
+    
 
